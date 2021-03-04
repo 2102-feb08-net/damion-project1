fda4f5de79bac2670137617442df5aabc586ee16
Damion Silver <drsilver@alumni.uncg.edu>
2021-03-03 22:30:53 -0500

    progress towards pulling customers by email

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace Models
 { 
 
     public class User{
-        public int Id { get; set; }
+        
+        [JsonIgnore] public int Id { get; set; }
         public string FirstName { get; set; }
         public string Lastname { get; set; }
-        public string Role { get; set; }
+         [JsonIgnore]public string Role { get; set; }
         public string Email { get; set; }
 
-        public string Password { get; set; }
+        [JsonIgnore] public string Password { get; set; }
 
 
         public User()
